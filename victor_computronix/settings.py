68e24a51a8cd436f114d9111b6dfab7c49308921
victorjanees <96464330+victorjanees@users.noreply.github.com>
2024-01-04 23:45:42 +0530

    Update settings.py

--- victor_computronix/settings.py
+++ victor_computronix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(5rxmb^@ab8qpocgub+g#s7m5y1a&@59ugurp+7c#@3iy^s82a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['VictorJanees.pythonanywhere.com']
 
 
 # Application definition
